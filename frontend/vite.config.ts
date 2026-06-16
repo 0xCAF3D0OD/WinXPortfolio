5cc1c09375d9f22b9c86e517e27dc46c16a4df75
kdi-noce <kevin.dinocera@protonmail.com>
2026-06-16 22:54:19 +0200

    fix: remove useless path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-website/',
+  base: '/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
